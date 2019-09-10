8fd9e0cec358b8e98c0475eeb964fef64e236cb1
Joshua <joshuatundag@gmail.com>
2019-09-10 13:22:36 +0800

    hide creds

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import { Provider } from 'react-redux'
 import store from '@/store'
 
 var config = {
-    apiKey: "AIzaSyBoXZ2sE9_KtJmOIwwpQ7c-nP0xoacrxeQ",
-    authDomain: "baby-gi-514cc.firebaseapp.com",
-    databaseURL: "https://baby-gi-514cc.firebaseio.com",
-    projectId: "baby-gi-514cc",
-    storageBucket: "",
-    messagingSenderId: "504164360105"
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL: process.env.FB_DB_URL,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MSG_SENDER_ID
 }
 firebase.initializeApp(config)
 
